6f45d9983d4a3ce83a611a9e5047756e607a7bbf
Sergey Golubev <qqsergeyqq@gmail.com>
2022-01-31 15:13:38 +0700

    First commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField()
+    description = models.TextField()
+    # image = models.ImageField(upload_to='portfolio/images/')
+    # url = models.URLField(blank=True)
+    def __str__(self):
+        return self.title
